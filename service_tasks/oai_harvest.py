5a9accf8d2268feb52141eb37f32e046bae73c95
Theodor Tolstoy <ttolstoy@ebsco.com>
2020-10-01 15:40:22 +0200

    create lightweight OAI harvester

--- service_tasks/oai_harvest.py
+++ service_tasks/oai_harvest.py
@@ -1,51 +1,69 @@
 
 import argparse
+import os
 import pathlib
 import json
 from abc import abstractmethod
-
+from sickle import Sickle
 import requests
+import codecs
 
 from service_tasks.service_task_base import ServiceTaskBase
 
 
-class ElasticLoader(ServiceTaskBase):
+class OAIHarvest(ServiceTaskBase):
     def __init__(self, args):
-        self.file_to_index = args.file_to_index
-        self.index_name = args.index_name
+        self.set_name = "all"
+        self.base_uri = args.base_uri
+        self.from_date = args.from_date
+        self.results_folder = args.results_folder
+        self.metadata_prefix = args.metadata_prefix
 
     def do_work(self):
-        payload = []
-        index = 0
-        with open(self.file_to_index) as datafile:
-            for line in datafile:
-                index += 1
-                payload.append(json.dumps(
-                    {"index": {"_index": self.index_name, "_id": index}}))
-                payload.append('\n')
-                payload.append(line)
-                if index % 500 == 0:
-                    print(f"posting {index}")
-                    response = requests.post('http://127.0.0.1:9200/_bulk',
-                                             data=''.join(payload) + '\n',
-                                             headers={"Content-Type": 'application/x-ndjson'})
-                    print(response.status_code, flush=True)
-                    if (str(response.status_code).startswith('4') or
-                            str(response.status_code).startswith('5')):
-                        print(response.text)
-                        print(json.dumps(response.json))
-                    payload = []
+        sickle = Sickle(self.base_uri, timeout=36000)
+        identify = sickle.Identify()
+        print(identify.repositoryName)
+        formats = [f.metadataPrefix for f in sickle.ListMetadataFormats()]
+        if self.metadata_prefix not in formats:
+            print(f"{self.metadata_prefix} is not amongst accepted metadata formats ({formats}")
+            raise ValueError("metadata format")
+        records = sickle.ListRecords(metadataPrefix=self.metadata_prefix, set='all')
+        i = 0
+        with codecs.open(os.path.join(self.results_folder, f"{i}.json"), "w+","utf-8") as harvest_file:
+            for record in records:
+                i+= 1
+                if i % 10 == 0:
+                    print(i)
+
+                try:
+                    harvest_file.write(record.raw.replace("<marc:","<").replace("</marc:","</"))
+                except Exception as ee:
+                    print(ee)
+                    print(record.raw)
+        print("Finished!")
+
+
 
     @staticmethod
     @abstractmethod
     def add_arguments(parser):
         ServiceTaskBase.add_argument(parser,
-                                     "file_to_index",
-                                     "File to load into Elasticsearch",
-                                     "FileChooser")
+                                     "results_folder",
+                                     "Folder where results are saved",
+                                     "DirChooser")
         ServiceTaskBase.add_argument(parser,
-                                     "index_name",
-                                     "Name of the Elastic Index",""
+                                     "base_uri",
+                                     "Base uri of the OAI server",""
                                      )
+
+        ServiceTaskBase.add_argument(parser,
+                                     "metadata_prefix",
+                                     "Metadata prefix",""
+                                     )
+        ServiceTaskBase.add_argument(parser,
+                                     "from_date",
+                                     "From date",""
+                                     )
+
 
 
