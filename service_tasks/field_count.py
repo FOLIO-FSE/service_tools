8e9ebbfa4cc835b9c603b4eea595a5882b96a2a4
Kyle Banerjee <kyle.banerjee@gmail.com>
2020-12-30 12:15:21 -0800

    add field counter for finding frequency of fields

---
+++ service_tasks/field_count.py
@@ -0,0 +1,26 @@
+from service_tasks.service_task_base import ServiceTaskBase, abstractmethod
+import sys, csv
+import pandas as pd
+
+class CountOccurrences(ServiceTaskBase):
+    def __init__(self, args):
+        self.csv_file = args.csv_file
+        self.field_wanted = args.field_wanted
+
+    def do_work(self):
+        print(f"Occurrences of {self.field_wanted} in reverse frequency order") 
+        field_values = pd.read_csv(self.csv_file)
+        print(field_values[self.field_wanted].value_counts())
+
+            
+    @staticmethod
+    @abstractmethod
+    def add_arguments(parser):
+        ServiceTaskBase.add_argument(parser, "csv_file", "Delimited file containing field to analyze", "FileChooser")
+        ServiceTaskBase.add_argument(parser, "field_wanted", "Name of field to analyze, must be at top of column", "")
+
+    @staticmethod
+    @abstractmethod
+    def add_cli_arguments(parser):
+        ServiceTaskBase.add_argument(parser, "csv_file", "Delimited file containing field to analyze", "FileChooser")
+        ServiceTaskBase.add_argument(parser, "field_wanted", "Name of field to analyze, must be at top of column", "")
