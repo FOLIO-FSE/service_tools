e554b537fea5058beeb0e910af8ad320b40e9723
Lisa Sjogren <lsjogren@corp.epnet.com>
2021-01-19 17:47:03 +0100

    print output to md file

--- service_tasks/visualize_csv_data.py
+++ service_tasks/visualize_csv_data.py
@@ -1,6 +1,7 @@
 import csv
 import pandas
 import matplotlib.pyplot as plt
+from mdutils.mdutils import MdUtils
 from service_tasks.service_task_base import ServiceTaskBase
 
 
@@ -10,54 +11,87 @@ class VisualizeCsvData(ServiceTaskBase):
         print("Let's get started!")
         self.csv_file = args.csv_file
         self.save_to_folder = args.save_to_folder
+        self.mdFile = MdUtils(file_name=self.save_to_folder + "/" + "report.md", title='ILS data field analysis')
+
 
     def do_work(self):
         with open(self.csv_file) as file:
+            print("Analyzing data...")
             data = pandas.read_csv(file, sep=",")
+
+        self.mdFile.new_header(level=1, title='ILS data field analysis')
+
+        self.mdFile.new_paragraph("Scroll down to learn about your legacy data.")
 
         # Create a bar chart showing how many times a field occurs in the data
         plt.rcParams["font.size"] = 12.0
         data.count().plot.barh(figsize=(10, 10), color = "royalblue")
         plt.title("Field occurences")
-        plt.savefig(save_to_folder + "field_occurences.svg")
 
-        for column in data.columns:
+        filepath = self.save_to_folder + "/" + "field_occurences.svg"
+        plt.savefig(filepath)
+        plt.close()
+
+        # Print overview to md file
+        self.mdFile.new_header(level=2, title="Data overview")
+        self.mdFile.new_paragraph(f"Your file contains {len(data.index)} rows (records) and {len(data.columns)} columns (fields). These are the present fields:")
+        self.mdFile.new_paragraph(f"These are the present fields:")
+        for name in data.columns.values:
+            self.mdFile.new_line(str(name))
+
+        self.mdFile.new_header(level=2, title="A closer look at the different fields")
+
+
+        print("Making pie charts...")
+
         # Show number of occurences for each unique value per column (truncated if many)
-        value_counts = data[column].value_counts()
-        value_names_raw = value_counts.index.tolist()
-        rint(f"{column}\n{value_counts}\n\n")    
-        
-        if value_counts.any():
-            value_names = [str(name).replace("$","|") for name in value_names_raw]
-            # Creating plot 
-            fig = plt.figure(figsize =(13, 10))
-            plt.title(column)
-            pieces, values = plt.pie(value_counts, labels = value_names, colors = plt.cm.tab20.colors)
-            plt.legend(pieces, value_names[:10], loc="best")
+        for column in data.columns:
+            value_counts = data[column].value_counts(ascending=True)
+            value_names_raw = value_counts.index.tolist()
+            self.mdFile.new_header(level=3, title=column)
+            
+            if value_counts.any():
+                value_names = [str(name).replace("$","|") for name in value_names_raw]
+                # Creating plot 
+                plt.figure(figsize =(13, 10))
+                plt.title(column)
+                plt.pie(value_counts, labels = value_names, colors = plt.cm.tab20.colors)
+                plt.legend(value_names, loc="best", ncol = 2) 
 
-            # Save plot as svg image
-            filename = column.replace(" ","_") + ".svg"
-            plt.savefig(save_to_folder + filename)
+                # Save plot as svg image
+                filename = column.replace(" ","_") + ".svg"
+                filepath = self.save_to_folder + "/" + filename
+                plt.savefig(filepath)
+                plt.close()
+
+                
+                self.mdFile.new_line(str(value_counts))
+                
+                self.mdFile.new_line(self.mdFile.new_inline_image(text= "Chart", path=filename))
+    
+
+
+        self.mdFile.new_table_of_contents(table_title='Contents', depth=3)
+        self.mdFile.create_md_file()
+
+
 
     @staticmethod
     def add_arguments(sub_parser):
-        ServiceTaskBase.add_common_arguments(sub_parser)
-
         ServiceTaskBase.add_argument(sub_parser,
                                      "csv_file",
                                      "The csv file to be analyzed. All rows must have the same number of columns.",
                                      "FileChooser")
         ServiceTaskBase.add_argument(sub_parser,
                                      "save_to_folder",
-                                     "Folder where you want the output to be saved. E.g. c:/thisplace/thatfolder",
-                                     "FileChooser")
+                                     "Folder where you want the output to be saved.",
+                                     "DirChooser")
 
     @staticmethod
     def add_cli_arguments(sub_parser):
-        ServiceTaskBase.add_common_arguments(sub_parser)
         ServiceTaskBase.add_cli_argument(sub_parser,
                                          "csv_file",
                                          "The csv file to be analyzed. All rows must have the same number of columns. c:/thisplace/thatfolder")
-        ServiceTaskBase.add_argument(sub_parser,
+        ServiceTaskBase.add_cli_argument(sub_parser,
                                      "save_to_folder",
-                                     "Folder where you want the output to be saved."
+                                     "Folder where you want the output to be saved.")
