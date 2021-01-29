ac3c9f94e15b61a591de4e80d4fdfd5d7358c91f
Theodor Tolstoy <ttolstoy@corp.epnet.com>
2021-01-29 13:14:08 +0100

    update user migration and cli args

--- user_migration/mappers/default.py
+++ user_migration/mappers/default.py
@@ -2,160 +2,168 @@ import csv
 import json
 import re
 import uuid
+from dateutil.parser import parse
+from typing import Dict
 
 from folioclient import FolioClient
 
 from user_migration.mappers.mapper_base import MapperBase
+
 
 class Default(MapperBase):
     def __init__(self, folio_client: FolioClient, args):
         super().__init__(folio_client)
         self.args = args
         self.user_schema = MapperBase.get_user_schema()
+        with open(args.mapping_file_path) as json_file:
+            self.user_map = json.load(json_file)
+        self.legacy_id_map: Dict[str, str] = {}
+        self.ids_dict: Dict[str, set] = {}
+        self.use_map = True
 
-    def do_map(self, legacy_user_dict):
+    def do_map(self, legacy_user):
+        # raise NotImplementedError("Create ID-Legacy ID Mapping file!")
+        # raise NotImplementedError("Check for ID duplicates (barcodes, externalsystemID:s, usernames, emails?, ")
         folio_user = self.instantiate_user()
-        legacy_user = legacy_user_dict["data"]
+        self.add_to_migration_report("Users per patron type", str(self.get_prop(legacy_user, "patronGroup")))
+        for prop_name, prop in self.user_schema["properties"].items():
+            if prop.get("description", "") == "Deprecated":
+                self.report_folio_mapping(f"{prop_name} (deprecated)", False, True)
+                continue
+            if prop_name == "metadata":
+                self.report_folio_mapping(f"{prop_name}", True, False)
+                continue
+            if prop["type"] == "object":
+                folio_user[prop_name] = {}
+                prop_key = prop_name
+                if "properties" in prop:
+                    for sub_prop_name, sub_prop in prop["properties"].items():
+                        sub_prop_key = prop_key + "." + sub_prop_name
+                        if "properties" in sub_prop:
+                            for sub_prop_name2, sub_prop2 in sub_prop["properties"].items():
+                                sub_prop_key2 = sub_prop_key + "." + sub_prop_name2
+                                if sub_prop2["type"] == "array":
+                                    print(f"Array: {sub_prop_key2} ")
+                        elif sub_prop["type"] == "array":
+                            folio_user[prop_name][sub_prop_name] = []
+                            for i in range(0, 5):
+                                if sub_prop["items"]["type"] == "object":
+                                    temp = {}
+                                    for sub_prop_name2, sub_prop2 in sub_prop["items"]["properties"].items():
+                                        temp[sub_prop_name2] = self.get_prop(legacy_user,
+                                                                             sub_prop_key + "." + sub_prop_name2, i)
+                                    if all(value == "" for key, value in temp.items() if key not in ["id", "primaryAddress"]):
+                                        continue
+                                    folio_user[prop_name][sub_prop_name].append(temp)
+                                else:
+                                    mkey = sub_prop_key + "." + sub_prop_name2
+                                    folio_user[prop_name][sub_prop_name] = self.get_prop(legacy_user,
+                                                                                         mkey,
+                                                                                         i)
 
-        self.add_to_migration_report("Users per patron type", str(legacy_user["patronGroup"]))
-        mapped_legacy_props = []
+                        else:
+                            folio_user[prop_name][sub_prop_name] = self.get_prop(legacy_user, sub_prop_key)
 
-        for prop in self.user_schema["properties"]:
-            if prop in legacy_user:  # is there a match in the csv?
-                if legacy_user[prop].strip():  # Match! Lets report this
-                    folio_user[prop] = legacy_user[prop]
-                    mapped_legacy_props.append(prop)
-                    self.report_legacy_mapping(prop, True, False)
-                    self.report_folio_mapping(prop, True, False)
-                else:  # Match but empty field. Lets report this
-                    mapped_legacy_props.append(prop)
-                    self.report_legacy_mapping(prop, True, True)
-                    self.report_folio_mapping(prop, True, True)
-            elif prop == "customFields":
-                custom_fields = [c for c in legacy_user if c.startswith("customField")]
-                if any(custom_fields):
-                    for custom_field in custom_fields:
-                        folio_user["customFields"][custom_field.split('.')[1]] = legacy_user[custom_field]
-                        self.add_to_migration_report("General",
-                                             f"Custom field {custom_field.split('.')[1]} added")
-                        self.report_legacy_mapping(prop, True, False)
-                        self.report_folio_mapping(prop, True, False)
+            elif prop["type"] == "array":
+                # handle departments
+                self.report_folio_mapping(f"Unhandled array: {prop_name}", False)
+
             else:
-                self.report_folio_mapping(prop, False, False)
-                # self.report_legacy_mapping(prop, False, False)
-        for prop in self.user_schema['properties']['personal']['properties']:
-            if prop in legacy_user:  # is there a match in the csv?
-                if legacy_user[prop].strip():  # Match! Lets report this
-                    folio_user['personal'][prop] = legacy_user[prop]
-                    mapped_legacy_props.append(prop)
-                    self.report_legacy_mapping(f"{prop}", True, False)
-                    self.report_folio_mapping(f"personal.{prop}", True, False)
-                else:  # Match but empty field. Lets report this
-                    mapped_legacy_props.append(prop)
-                    self.report_legacy_mapping(f"{prop}", True, True)
-                    self.report_folio_mapping(f"personal.{prop}", True, True)
-            else:
-                self.report_folio_mapping(f"personal.{prop}", False, False)
-        for prop in legacy_user:
+                self.map_basic_props(legacy_user, prop_name, folio_user)
+                """ elif prop == "customFields":
+                    custom_fields = [c for c in legacy_user if c.startswith("customField")]
+                    if any(custom_fields):
+                        for custom_field in custom_fields:
+                            folio_user["customFields"][custom_field.split('.')[1]] = legacy_user[custom_field]
+                            self.add_to_migration_report("General",
+                                                         f"Custom field {custom_field.split('.')[1]} added")
+                            self.report_legacy_mapping(prop, True, False)
+                            self.report_folio_mapping(prop, True, False) """
+
+            """ for prop in legacy_user:
             if prop not in mapped_legacy_props:
                 if legacy_user[prop].strip():
                     self.report_legacy_mapping(f"{prop}", False, False)
                 else:
-                    self.report_legacy_mapping(f"{prop}", False, True)
-        folio_user['patronGroup'] = legacy_user['patronGroup']
-        self.add_to_migration_report("Users by FOLIO Patron Group", folio_user['patronGroup'])
-        self.handle_addresses(folio_user, legacy_user_dict)
-        self.validate(folio_user)
+                    self.report_legacy_mapping(f"{prop}", False, True)"""
+
+        # folio_user['patronGroup'] = self.get_prop(legacy_user, 'patronGroup')
+        # self.add_to_migration_report("Users by FOLIO Patron Group", folio_user['patronGroup'])
+        # self.handle_addresses(folio_user, legacy_user_dict)
+        # self.validate(folio_user)
         if self.args.temp_email:
             self.add_to_migration_report("General", f"Replaced email with {self.args.temp_email}")
             folio_user["personal"]["email"] = self.args.temp_email
+        folio_user["personal"]["preferredContactTypeId"] = self.user_map["preferredContactTypeId"]
+        folio_user["active"] = True
+        del folio_user["personal"]["preferredFirstName"]
+        del folio_user["tags"]
+
         return folio_user
 
-    def handle_addresses(self, folio_user, legacy_user_dict):
-        a = 0
-        folio_user['personal']['addresses'] = list()
-        primary_set = False
-        mapped_types = []
-        addresses = legacy_user_dict["addresses"]
-        address_types = list([a['addressTypeId'] for a in addresses])
-        address_types.sort()
-        self.add_to_migration_report("Address type breakdown", "-".join(address_types))
-        self.add_to_migration_report("Address type breakdown", f"Patrons with {len(addresses)} address")
-        email = ""
-        for legacy_address in addresses:
-            self.add_to_migration_report("Address Types from Legacy system", f"All addresses, total")
-            is_duplicate = legacy_address['addressTypeId'].lower() in mapped_types
-            if is_duplicate:
-                self.add_to_migration_report("Address Types from Legacy system",
-                                             f"Duplicate of {legacy_address['addressTypeId']}")
-            else:
-                self.add_to_migration_report("Address Types from Legacy system", f"{legacy_address['addressTypeId']}")
-                mapped_types.append(legacy_address['addressTypeId'].lower())
-                folio_address = {}
-                primary = False
-                a += 1
-                for prop in self.user_schema['properties']['personal']['properties']['addresses']['items'][
-                    'properties']:
-                    if prop in legacy_address:  # is there a match in the csv?
-                        if legacy_address[prop].strip():  # Match! Lets report this
-                            folio_address[prop] = legacy_address[prop]
-                            self.report_legacy_mapping(f"address.{a}.{prop}", True, False)
-                            self.report_folio_mapping(f"personal.addresses.{a}.{prop}", True, False)
-                        else:  # Match but empty field. Lets report this
-                            self.report_legacy_mapping(f"address.{a}.{prop}", True, True)
-                            self.report_folio_mapping(f"personal.addresses.{a}.{prop}", True, True)
-                    else:
-                        self.report_folio_mapping(f"personal.addresses.{a}.{prop}", False, False)
-                        self.report_legacy_mapping(f"address.{a}.{prop}", False, False)
-                    if str(legacy_address['addressTypeId']).lower() == "temporary":
-                        folio_address['addressTypeId'] = "Campus"  # campus
-                        if legacy_address['primaryAddress'].lower() == "temporary":
-                            primary = True if not primary_set else False
-                            primary_set = True
-                    elif str(legacy_address['addressTypeId']).lower() == "permanent":
-                        if legacy_address['primaryAddress'].lower() == "permanent":
-                            primary = True if not primary_set else False
-                            primary_set = True
-                        folio_address['addressTypeId'] = "Home"  # Home
-                    folio_address['primaryAddress'] = primary
-                    self.add_to_migration_report("Primary addresses", str(primary))
-                    self.add_to_migration_report("Country Codes", folio_address.get('countryId', 'None'))
-                folio_address['countryId'] = folio_address.get('countryId', 'None')
-            folio_user['personal']['addresses'].append(folio_address)
-
-        if not folio_user['personal'].get('email', '').strip() and email:
-            folio_user['personal']['email'] = email
-            self.add_to_migration_report("Email comes from", "addressLine1")
+    def map_basic_props(self, legacy_user, prop, folio_user):
+        if self.has_property(legacy_user, prop):  # is there a match in the csv?
+            if self.get_prop(legacy_user, prop).strip():
+                folio_user[prop] = self.get_prop(legacy_user, prop)
+                self.report_legacy_mapping(self.legacy_property(prop), True, False)
+                self.report_folio_mapping(prop, True, False)
+            else:  # Match but empty field. Lets report this
+                self.report_legacy_mapping(self.legacy_property(prop), True, True)
+                self.report_folio_mapping(prop, True, True)
         else:
-            self.add_to_migration_report("Email comes from", "email field")
-        if folio_user['personal']['addresses'] and not any(
-                a for a in folio_user['personal']['addresses'] if a['primaryAddress']):
-            self.add_to_migration_report("Primary addresses", "Unhandled, set first as primary")
-            folio_user['personal']['addresses'][0]['primaryAddress'] = True
+            self.report_folio_mapping(prop, False)
 
     def get_users(self, source_file):
         address_fields = ["countryId", "addressLine1", "addressLine2", "city", "region", "postalCode",
                           "addressTypeId", "primaryAddress"]
         csv.register_dialect("tsv", delimiter="\t")
-        reader = csv.DictReader(source_file, dialect='tsv')
+        reader = csv.DictReader(source_file)
         current_user = {}
         current_user_id = ""
         for row in reader:
-            address = {key: value for (key, value) in row.items() if key in address_fields}
-            bad_email = re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", address['addressLine1']) and \
-                        address['addressTypeId'].lower() in ['temporary', 'permanent']
+            # return [row]
+            yield row
 
-            if row['username'] != current_user_id:  # new user, yield the old one.
-                old_user = current_user
-                first = False if current_user_id else True
-                current_user_id = row['username']
-                user_data = {key: value for (key, value) in row.items() if key not in address_fields}
-                current_user = {"data": user_data,
-                                "addresses": [address]}
-                if not first:
-                    yield old_user
-            elif row['username'] == current_user_id and current_user_id:
-                if not bad_email:
-                    current_user['addresses'].append(address)
-                else:
-                    self.add_to_migration_report("Emails that are listed as other addresses", bad_email)
+    def get_prop(self, user, folio_prop_name, i=0):
+        if self.use_map:
+            user_key = next((k["legacy_field"] for k in self.user_map["data"] if
+                             k["folio_field"].replace(f"[{i}]", "") == folio_prop_name), "")
+            if folio_prop_name == "personal.addresses.id":
+                return str(uuid.uuid4())
+            elif folio_prop_name == "expirationDate":
+                exp_date = parse(user[user_key], fuzzy=True)
+                return exp_date.isoformat()
+            elif folio_prop_name.strip() == "personal.addresses.primaryAddress":
+                return i == self.user_map["primaryAddressIndex"]
+            elif folio_prop_name == "personal.addresses.addressTypeId":
+                try:
+                    address_type_id = self.user_map["addressTypes"][i]
+                    self.report_folio_mapping(f"{folio_prop_name}", True, False)
+                    return address_type_id
+                except IndexError:
+                    return ""
+            elif user_key:
+                self.report_folio_mapping(f"{folio_prop_name}", True, False)
+                return user[user_key]
+            else:
+                self.report_folio_mapping(f"{folio_prop_name}", False, False)
+                return ""
+        else:
+            self.report_folio_mapping(f"{folio_prop_name}", True, False)
+            return user[folio_prop_name]
+
+    def has_property(self, user, folio_prop_name):
+        if self.use_map:
+            user_key = next((k["legacy_field"] for k in self.user_map["data"] if k["folio_field"] == folio_prop_name),
+                            "")
+            return user_key and user_key not in ["", "Not mapped"] and user.get(user_key,"")
+        else:
+            return folio_prop_name in user
+
+    def legacy_property(self, folio_prop):
+        if self.use_map:
+            return next(k["legacy_field"] for k in self.user_map["data"] if k["folio_field"] == folio_prop)
+        else:
+            return folio_prop
+
+
