4c131652d9a01df29a70be100c94899025b7d269
Theodor Tolstoy <ttolstoy@corp.epnet.com>
2021-03-19 15:30:56 +0100

    save failed batches to file

--- service_tasks/batch_poster.py
+++ service_tasks/batch_poster.py
@@ -1,4 +1,5 @@
 import json
+import os
 import traceback
 from abc import abstractmethod
 from datetime import datetime
@@ -9,6 +10,11 @@ from folioclient import FolioClient
 from service_tasks.service_task_base import ServiceTaskBase
 
 
+def write_failed_batch_to_file(batch, file):
+    for record in batch:
+        file.write(f"{json.dumps(record)}\n")
+
+
 class BatchPoster(ServiceTaskBase):
     def __init__(self, folio_client: FolioClient, args):
         super().__init__(folio_client)
@@ -17,8 +23,13 @@ class BatchPoster(ServiceTaskBase):
         self.api_path = list_objects()[args.object_name]
         self.object_name = args.object_name
         self.failed_objects = []
+        object_name_formatted = self.object_name.replace(" ", "").lower()
+        self.log_path = os.path.join(args.results_folder, f'{object_name_formatted}_posting.log')
+        self.failed_recs_path = os.path.join(args.results_folder, f'{object_name_formatted}_failed_records.json')
         self.batch_size = args.batch_size
         self.processed = 0
+        self.failed_batches = 0
+        self.failed_records = 0
         self.processed_rows = 0
         self.objects_file = args.objects_file
         self.users_per_group = {}
@@ -30,33 +41,52 @@ class BatchPoster(ServiceTaskBase):
         print("Starting....")
         batch = []
 
-        with open(self.objects_file) as rows:
-
-            for row in rows:
+        with open(self.objects_file) as rows, open(self.failed_recs_path) as failed_recs_file, open(
+                self.log_path) as log_file:
+            last_row = ""
+            for idx, row in enumerate(rows):
+                last_row = row
                 if self.processed_rows < self.start:
                     continue
-                try:
-                    if row.strip():
+                if row.strip():
+                    try:
                         self.processed_rows += 1
                         json_rec = json.loads(row.split("\t")[-1])
                         batch.append(json_rec)
                         if len(batch) == int(self.batch_size):
                             self.post_batch(batch)
                             batch = []
-                except Exception as exception:
-                    print(f"{exception} row failed", flush=True)
-                    batch = []
-                    traceback.print_exc()
-                    self.num_failures += 0
-                    if self.num_failures > 5:
-                        print(f"Exceeded number of failures at row {idx}")
-                        raise exception
+                    except UnicodeDecodeError as unicode_error:
+                        print("=========ERROR==============")
+                        print(f"{unicode_error} Posting failed. Encoding error reading file")
+                        print(f"Failing row, either the one shown here or the next row in {self.objects_file}")
+                        print(last_row)
+                        print("=========Stack trace==============")
+                        traceback.print_exc()
+                        print("=======================", flush=True)
+                    except Exception as exception:
+                        print("=========ERROR==============")
+                        print(f"Posting failed with the following error")
+                        print(f"{exception} Posting failed")
+                        print(f"Failing row, either the one shown here or the next row in {self.objects_file}")
+                        print(last_row)
+                        print("writing failed batch to file")
+                        self.failed_batches += 1
+                        self.failed_records += len(batch)
+                        write_failed_batch_to_file(batch, failed_recs_file)
+                        batch = []
+                        print("=========Stack trace==============")
+                        traceback.print_exc()
+                        print("=======================", flush=True)
+                        self.num_failures += 0
+                        if self.num_failures > 50:
+                            print(f"Exceeded number of failures at row {idx}")
+                            raise exception
             # Last batch
         self.post_batch(batch)
-        print(json.dumps(self.failed_objects), flush=True)
-        print(json.dumps(self.failed_ids, indent=4), flush=True)
+        print(f"{self.failed_records} failed records in {self.failed_batches} saved to {self.failed_recs_path}")
 
-    def post_batch(self, batch, repost=False):
+    def post_batch(self, batch):
         response = self.do_post(batch)
         if response.status_code == 201:
             print(
@@ -66,68 +96,18 @@ class BatchPoster(ServiceTaskBase):
                 flush=True,
             )
         elif response.status_code == 422:
-            print(f"{response.status_code}\t{response.text} "
-                  f"Request size: {get_req_size(response)} "
-                  f"{datetime.utcnow().isoformat()} UTC", flush=True)
             resp = json.loads(response.text)
-
-            for error in resp["errors"]:
-                self.failed_fields.add(error["parameters"][0]["key"])
-                self.failed_ids.append(error["parameters"][0]["value"])
-            print(f"1 {len(batch)}", flush=True)
-            self.handle_failed_batch(batch)
-            """if not repost:
-                self.handle_failed_batch(batch)
-            else:
-                raise Exception(f"Reposting despite handling. {self.failed_ids}")"""
-        elif response.status_code in [500, 413]:
-            # Error handling is sparse. Need to identify failing records
-            print(f"{response.status_code}\t"
-                  "Request size: {get_req_size(response)}"
-                  f"\n{response.text} "
-                  f"{datetime.utcnow().isoformat()} UTC", flush=True)
-            if not len(batch) == 1:
-                # split the batch in 2
-                my_chunks = chunks(batch, 2)
-                for chunk in my_chunks:
-                    print(f"chunks with {len(chunk)} objects. " "posting chunk...", flush=True)
-                    self.post_batch(chunk)
-            else:
-                print(
-                    f"Only one object left. Adding {batch[0]['id']} to failed_objects"
-                    , flush=True)
-                self.failed_objects = batch[0]
+            raise Exception(
+                f"HTTP {response.status_code}\t"
+                f"Request size: {get_req_size(response)}"
+                f"{datetime.utcnow().isoformat()} UTC\n"
+                f"{json.dumps(resp, indent=4)}")
         else:
             raise Exception(
-                f"ERROR! HTTP {response.status_code}\t{response.text} "
+                f"HTTP {response.status_code}\t"
                 f"Request size: {get_req_size(response)}"
-                f"{datetime.utcnow().isoformat()} UTC")
-
-    def handle_failed_batch(self, batch):
-        # new_batch = [f for f in batch]  # if f["instanceId"] not in self.failed_ids]
-        ff = list(self.failed_fields)
-        if "holdingsRecordId" in ff:
-            new_batch = [
-                f for f in batch if (f["holdingsRecordId"] not in self.failed_ids)
-            ]
-        elif "instanceId" in ff:
-            new_batch = [f for f in batch if f["instanceId"] not in self.failed_ids]
-        else:
-            raise Exception(f"unhandled failed fields: {ff}")
-        # print(f"2 {len(batch)}")
-        """
-        for it in batch:
-            batch_string = json.dumps(it)
-            for id in self.failed_ids:
-                if id in batch_string:
-                    print(f"id found {id}, removing.")
-                elif it not in new_batch:
-                    new_batch.append(it)"""
-        print(
-            f"re-posting new batch {len(self.failed_ids)} {len(batch)} {len(new_batch)}",
-            flush=True,
-        )
-        self.post_batch(new_batch, True)
+                f"{datetime.utcnow().isoformat()} UTC\n"
+                f"{response.text}")
 
     def do_post(self, batch):
         kind = list_objects()[self.object_name]
@@ -137,10 +117,6 @@ class BatchPoster(ServiceTaskBase):
             payload = {"records": list(batch), "totalRecords": len(batch)}
         else:
             payload = {kind["object_name"]: batch}
-        # print(json.dumps(payload, ensure_ascii=True))
-        # if self.first_batch:
-        #    print(json.dumps(payload))
-        #    self.first_batch = False
         return requests.post(
             url, data=json.dumps(payload), headers=self.folio_client.okapi_headers
         )
@@ -151,6 +127,8 @@ class BatchPoster(ServiceTaskBase):
         ServiceTaskBase.add_common_arguments(parser)
         ServiceTaskBase.add_argument(parser, "objects_file", "path data file", "FileChooser")
         ServiceTaskBase.add_argument(parser, "batch_size", "batch size", "")
+        ServiceTaskBase.add_argument(parser, "results_folder", "Folder where failing records "
+                                                               "and logs will be stored", "DirChooser")
         ServiceTaskBase.add_argument(parser, "object_name", "What objects to batch post", "Dropdown",
                                      choices=list(list_objects().keys()),
                                      )
@@ -161,6 +139,8 @@ class BatchPoster(ServiceTaskBase):
         ServiceTaskBase.add_common_arguments(parser)
         ServiceTaskBase.add_cli_argument(parser, "objects_file", "path data file")
         ServiceTaskBase.add_cli_argument(parser, "batch_size", "batch size")
+        ServiceTaskBase.add_cli_argument(parser, "results_folder", "Folder where failing records "
+                                                                   "and logs will be stored")
         ServiceTaskBase.add_cli_argument(parser, "object_name", "What objects to batch post",
                                          choices=list(list_objects().keys())
                                          )
