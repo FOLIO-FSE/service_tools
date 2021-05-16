a247d92cf79b80405d3cb48a57d9923ea2d6bd4d
Lisa Sjogren <lsjogren@corp.epnet.com>
2021-05-16 11:14:10 +0200

    fix output

--- service_tasks/create_from_model.py
+++ service_tasks/create_from_model.py
@@ -1,6 +1,6 @@
 import csv
 import json
-import pandas as pd
+import uuid
 import re
 
 from service_tasks.service_task_base import ServiceTaskBase
@@ -12,9 +12,10 @@ class CreateRecordFromModel(ServiceTaskBase):
     def __init__(self, args):
         print("\n~*~*~\nLet's get started!")
         self.data_file = args.data_file
-        self.save_to_folder = args.save_to_folder
+        self.result_file = args.save_to_folder + "/modelled_records.json"
         self.model_file = args.model_record
         self.dedupe = args.dedupe
+        self.map_out = args.save_to_folder + "/id_map.json"
 
 
     def do_work(self):
@@ -23,10 +24,10 @@ class CreateRecordFromModel(ServiceTaskBase):
             records = []
             rows_processed = 0
             unique_processed = []
+            id_map = {}
             
             # Open the model and identify the values that should be replaced with data
-            model = json.load(model_file)
-            model = str(model)
+            model = str(json.load(model_file))
             fields_to_replace = re.findall("\{\{[a-zA-Z0-9_]*\}\}", model)
 
             # Read the data into a data frame
@@ -36,25 +37,40 @@ class CreateRecordFromModel(ServiceTaskBase):
             for row in data:
                 try:
                     rows_processed += 1
+                    if rows_processed % 100 == 0:
+                        print(rows_processed)
                     # Create a new record based on the model
                     if row[self.dedupe] not in unique_processed:
                         record = model
 
                         # Loop through the fields, and replace with values from the data
                         for field in fields_to_replace:
-                            record = record.replace(field, row[field])
+                            name = field[2:-2]
+                            if name.startswith("new_uuid"):
+                                this_uuid = str(uuid.uuid4())
+                                record = record.replace(field, this_uuid)
+                                id_map[row[self.dedupe]] = {name : this_uuid}
+                            else:
+                                record = record.replace(field, row[field])
 
                         # Add record to list and list of processed records
+                        record = eval(record)
                         records.append(record)
                         unique_processed.append(row[self.dedupe])
                 except KeyError as e:
                     print(f"Make sure that this field is in your headers AND model record: {e}")
-
+                    raise
+            
+                
+            with open(self.result_file, "w") as results_out, open(self.map_out, "w") as map_out:
+                print(json.dumps(records, ensure_ascii=False), file=results_out)
+                print(id_map, file=map_out)
 
 
             print(f"Number of records processed: {rows_processed}")
             print(f"Number of records created: {len(records)}")
             print(*records[:2], sep = "\n")
+
 
     @staticmethod
     def add_arguments(sub_parser):
