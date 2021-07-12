d089232c20348aea0770c3507ef9118c03651e0e
fontanka16 <github.teddes@tolstoy.se>
2021-07-12 16:17:58 +0200

    fix bug, add failed_files

--- service_tasks/gvsu_add_callnumbers_to_holdings.py
+++ service_tasks/gvsu_add_callnumbers_to_holdings.py
@@ -1,7 +1,9 @@
 import csv
 import json
 import logging
+import time
 from abc import abstractmethod
+from pathlib import Path
 
 import requests
 
@@ -17,33 +19,47 @@ class GVSUAddCallnumbersToHoldings(ServiceTaskBase):
         self.args = args
 
     def do_work(self):
-        api_path = "/inventory/instances"
-        hold_path = "/holdings-storage/holdings"
-        with open(self.args.input_file, 'r', encoding="utf-8") as input_file:
-            for idx, row in enumerate(InsensitiveDictReader(input_file, dialect="tsv")):
-                bib_id = row["record number(bibliographic)"]
-                if row.get("090", "") or row.get("050", "") or row.get("086", ""):
-                    query = f'?limit=100&query=(identifiers =/@value \"{bib_id}\")'
-                    objects = list(self.folio_client.folio_get_all(api_path, "instances", query))
-                    if len(objects) != 1:
-                        logging.warning(f"{len(objects)} Instances found in folio for {bib_id}. Skipping")
-                    else:
-                        instance_id = objects[0].get("id")
-                        hold_query = f"?limit=1000&query=instanceId=={instance_id}"
-                        holdings = list(self.folio_client.folio_get_all(hold_path, "holdingsRecords", hold_query))
-                        if not holdings:
-                            logging.warning(
-                                f"{len(holdings)} holdings found for Instance {instance_id} "
-                                f"(Bib id: {bib_id}). Skipping")
+        time_stamp = time.strftime("%Y%m%d-%H%M%S")
+        failed_path = Path(self.args.results_folder) / f'failed_recs_{time_stamp}.tsv'
+        with open(failed_path, "w+") as failed_file:
+            failed_writer = None
+            api_path = "/inventory/instances"
+            hold_path = "/holdings-storage/holdings"
+            with open(self.args.input_file, 'r', encoding="utf-8") as input_file:
+                for idx, row in enumerate(InsensitiveDictReader(input_file, dialect="tsv")):
+                    try:
+                        if idx == 0:
+                            failed_writer = csv.DictWriter(failed_file, dialect='tsv', fieldnames=row.keys())
+                            failed_writer.writeheader()
+                        bib_id = row["record number(bibliographic)"]
+                        if row.get("090", "") or row.get("050", "") or row.get("086", ""):
+                            query = f'?limit=100&query=(identifiers =/@value \"{bib_id}\")'
+                            objects = list(self.folio_client.folio_get_all(api_path, "instances", query))
+                            if len(objects) != 1:
+                                logging.warning(f"{len(objects)} Instances found in folio for {bib_id}. Skipping")
+                            else:
+                                instance_id = objects[0].get("id")
+                                hold_query = f"?limit=1000&query=instanceId=={instance_id}"
+                                holdings = list(
+                                    self.folio_client.folio_get_all(hold_path, "holdingsRecords", hold_query))
+                                if not holdings:
+                                    logging.warning(
+                                        f"{len(holdings)} holdings found for Instance {instance_id} "
+                                        f"(Bib id: {bib_id}). Skipping")
+                                else:
+                                    for holding in holdings:
+                                        self.update_and_put_holdings(bib_id, hold_path, holding, instance_id, row,
+                                                                     failed_writer)
                         else:
-                            for holding in holdings:
-                                self.update_and_put_holdings(bib_id, hold_path, holding, holdings, instance_id, row)
-                else:
-                    self.add_stats("Rows in file w/o any callNumbers")
-                if idx % 10 == 0:
-                    print(f'{idx + 1} records processed. {json.dumps(self.stats, sort_keys=True)}')
+                            self.add_stats("Rows in file w/o any callNumbers")
+                    except Exception as ee:
+                        failed_writer.writerow(row)
+                        logging.error(f"{ee}")
 
-    def update_and_put_holdings(self, bib_id, hold_path, holding, holdings, instance_id, row):
+                    if idx % 10 == 0:
+                        print(f'{idx + 1} records processed. {json.dumps(self.stats, sort_keys=True)}')
+
+    def update_and_put_holdings(self, bib_id, hold_path, holding, instance_id, row, failed_writer):
         try:
             prev_call_number = holding.get("callNumber", "")
             lc_callnumber_type = "95467209-6d7b-468b-94df-0f5d7ad2747d"
@@ -61,7 +77,7 @@ class GVSUAddCallnumbersToHoldings(ServiceTaskBase):
                 holding["callNumber"] = row.get("050")
                 holding["callNumberTypeId"] = lc_callnumber_type
             url = f'{self.folio_client.okapi_url}{hold_path}/{holding["id"]}'
-            req = requests.put(url, data=json.dumps(holdings[0]),
+            req = requests.put(url, data=json.dumps(holding),
                                headers=self.folio_client.okapi_headers)
             req.raise_for_status()
             self.add_stats("Successful updates")
@@ -70,6 +86,7 @@ class GVSUAddCallnumbersToHoldings(ServiceTaskBase):
                 f'{holding["callNumber"]} for {instance_id} (Holdings UUID: {holding["id"]})')
         except Exception as ee:
             self.add_stats("Failed updates")
+            failed_writer.writerow(row)
             logging.error(
                 f'PUT failed: {url}\t{ee} for {instance_id} (Bib id: {bib_id}) '
                 f'(Holdings UUID: {holding["id"]})')
